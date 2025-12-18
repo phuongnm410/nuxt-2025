4e19495bb326c3223d0f20fd482eb16f96bb7dcd
phuongnm <phuongnguyenminh@tech.admicro.vn>
2025-12-18 17:42:55 +0700

    add nuxt

---
+++ app/stores/website.ts
@@ -0,0 +1,17 @@
+
+import { defineStore } from 'pinia'
+
+export const useWebsiteStore = defineStore('websiteStore', {
+  state: () => ({
+    name: '',
+    description: '',
+  }),
+  actions: {
+    async fetch () {
+      const infos = await $fetch('https://api.nuxt.com/modules/pinia')
+
+    //   this.name = infos.name
+    //   this.description = infos.description
+    },
+  },
+})
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,5 +10,8 @@ export default defineNuxtConfig({
       '@tailwindcss/postcss': {},
       autoprefixer: {}
     }
-  }
+  },
+  modules: [
+    '@pinia/nuxt',
+  ],
 })
